e75fb535dc03fb1794ddc2350528346b134adaa3
Maria Theresa <mariatheresahenriques@gmail.com>
2020-11-11 08:32:44 -0300

    corrigindo para 64bits

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,11 +1,11 @@
 module InstructionMemory (addressPC, instruction);
-  input wire [31:0]addressPC;
-  output reg [31:0]instruction;
+  input wire [63:0]addressPC;
+  output reg [63:0]instruction;
 
   always @ (*) begin
     case (addressPC)
 
-      32'b00000000000000000000000000000000: instruction <= 32'b00000000000100000000000100110011;// Tipo R - add x2, x0, x1 
+      64'b0000000000000000000000000000000000000000000000000000000000000000: instruction <= 64'b0000000000000000000000000000000000000000000100000000000100110011;// Tipo R - add x2, x0, x1
 
     endcase
   end
