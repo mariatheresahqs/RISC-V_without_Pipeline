69d4ff5cec9e287d251d87e83a58ce8eb8fd2009
Maria Theresa <mariatheresahenriques@gmail.com>
2020-11-11 08:24:20 -0300

    alterando para 64bits

--- PC.v
+++ PC.v
@@ -1,11 +1,11 @@
 module PC (PC, reset, nextPC);
   input wire reset;
-  output reg [31:0]PC;
-  output reg [31:0]nextPC;
+  output reg [63:0]PC;
+  output reg [63:0]nextPC;
 
   always @ (*) begin
     if(reset) begin // inicializar (ou resetar) valores de PC com 0s
-      nextPC<= 32'd0;
+      nextPC<= 64'd0;
     end
     else begin // caso contrario NextPC passara a ser o valor de PC
       nextPC <= PC;
@@ -17,10 +17,10 @@ endmodule
 // Atualizacao do PC quando nao ha branch: PC + 4 (proxima instrucao)
 //-----------------------------------------------------------------
 module Sum4 (PC, sum);
-  input wire [31:0]PC;
-  output reg [31:0]sum;
+  input wire [63:0]PC;
+  output reg [63:0]sum;
   always @ (*) begin
-    sum <= PC + 32'd4;
+    sum <= PC + 64'd4;
   end
 endmodule
 
@@ -28,17 +28,17 @@ endmodule
 // Atualizacao de PC quando a instrucao de desvio for verdadeira (beq): PC + ShiftLeft = Sum
 //-----------------------------------------------------------------
 module ShiftLeft (signExtend, result);
-  input wire [31:0]signExtend;
-  output reg [31:0]result;
+  input wire [63:0]signExtend;
+  output reg [63:0]result;
   always @ (*) begin
     result <= signExtend  <<  1; //Shift Left, Logical (fill with zero)
   end
 endmodule
 
 module ResultPC (PC, shiftValue, sum, ANDBranch, clk);  // desempenhara o papel do AddSum(pc + shiftValue) + Mux(ANDBranch "flag")
-  input wire [31:0] PC, shiftValue; // valores que sao recebidos pelo AddSum
+  input wire [63:0] PC, shiftValue; // valores que sao recebidos pelo AddSum
   input wire ANDBranch, clk;
-  output reg [31:0] sum;
+  output reg [63:0] sum;
   always @ (posedge clk) begin
     if(ANDBranch) begin
       sum <= PC + shiftValue;
@@ -48,3 +48,4 @@ module ResultPC (PC, shiftValue, sum, ANDBranch, clk);  // desempenhara o papel 
     end
   end
 endmodule
+
