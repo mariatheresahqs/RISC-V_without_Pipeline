de4ad473edf346fe65b649fa058c099aa743ff13
Maria Theresa <mariatheresahenriques@gmail.com>
2020-11-11 07:25:29 -0300

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,36 +1,50 @@
-module PC (PC, Reset, NextPC);
-  input wire Reset;
+module PC (PC, reset, nextPC);
+  input wire reset;
   output reg [31:0]PC;
-  output reg [31:0]NextPC;
+  output reg [31:0]nextPC;
 
   always @ (*) begin
-    if(Reset) begin // inicializar (ou resetar) valores de PC com 0s
-      NextPC<= 32'd0;
+    if(reset) begin // inicializar (ou resetar) valores de PC com 0s
+      nextPC<= 32'd0;
     end
     else begin // caso contrario NextPC passara a ser o valor de PC
-      NextPC <= PC;
+      nextPC <= PC;
     end
   end
 endmodule
 
-module Sum4 (PC, Sum); // atualizacao do PC quando nao ha desvio: PC + 4
+//-----------------------------------------------------------------
+// Atualizacao do PC quando nao ha branch: PC + 4 (proxima instrucao)
+//-----------------------------------------------------------------
+module Sum4 (PC, sum);
   input wire [31:0]PC;
-  output reg [31:0]Sum;
+  output reg [31:0]sum;
   always @ (*) begin
-    Sum <= PC + 32'd4;
+    sum <= PC + 32'd4;
   end
 endmodule
 
-module ResultPC (PC, ShiftValue, Sum, ANDBranch, clock); // atualizacao de PC quando a instrucao de desvio e verdadeira (beq)
-  input wire [31:0] PC, ShiftValue; // valores a serem recebidos pelo ADD SUM
-  input wire ANDBranch, clock; // valor de ANDBranch recebido pelo Mux
-  output reg [31:0] Sum;
-  always @ (posedge clock) begin
-    if(ANDBranch) begin // AndBranch verdadeiro = 1
-      Sum <= PC + ShiftValue;
+//-----------------------------------------------------------------
+// Atualizacao de PC quando a instrucao de desvio for verdadeira (beq): PC + ShiftLeft = Sum
+//-----------------------------------------------------------------
+module ShiftLeft (signExtend, result);
+  input wire [31:0]signExtend;
+  output reg [31:0]result;
+  always @ (*) begin
+    result <= signExtend  <<  1; //Shift Left, Logical (fill with zero)
+  end
+endmodule
+
+module ResultPC (PC, shiftValue, sum, ANDBranch, clk);  // desempenhara o papel do AddSum(pc + shiftValue) + Mux(ANDBranch "flag")
+  input wire [31:0] PC, shiftValue; // valores que sao recebidos pelo AddSum
+  input wire ANDBranch, clk;
+  output reg [31:0] sum;
+  always @ (posedge clk) begin
+    if(ANDBranch) begin
+      sum <= PC + shiftValue;
     end
-    else if(~ANDBranch) begin // AndBranch falso = 0
-      Sum <= PC; // PC ja foi atualizado no Sum4
+    else if(~ANDBranch) begin
+      sum <= PC;  // PC +4 (sum4)
     end
   end
 endmodule
